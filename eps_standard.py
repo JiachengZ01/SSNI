66f2363c19d9c79c979746f3b1c24bacf7259f21
YUHAOSUNABC <yuhaos1@student.unimelb.edu.au>
2025-03-29 19:59:39 +1100

    Update eps_standard.py, fix requirements.txt, add new scripts

--- eps_standard.py
+++ eps_standard.py
@@ -6,7 +6,8 @@ def load_dataset_eps(args):
             print("Using Score Norm as reweighting metric!")
             cf10_pt = f"Enter the path to the score file by running cifar10_eps_range.sh"
         else:
-            cf10_pt = f"Enter the path to the eps file by running cifar10_eps_range.sh"
+            # cf10_pt = f"Enter the path to the eps file by running cifar10_eps_range.sh"
+            cf10_pt = "/data/gpfs/projects/punim2205/ICLR2025/DBP/loaded_data/cifar10/sd121/5000_cifar10_eps.pt"
         cf10_eps_data = torch.load(cf10_pt)
 
         cf10_nat_eps_list = []
@@ -16,7 +17,8 @@ def load_dataset_eps(args):
         cf10_eps_standard = cf10_nat_eps_list
         return cf10_eps_standard
     elif args.dataset == 'imagenet':
-        imgnet_pt = f"Enter the path to the eps file by running imagenet_eps_range.sh"
+        # imgnet_pt = f"Enter the path to the eps file by running imagenet_eps_range.sh"
+        imgnet_pt = "/data/gpfs/projects/punim2205/ICLR2025/DBP/loaded_data/imagenet/sd121/5000_imagenet_eps.pt"
         imgnet_eps_data = torch.load(imgnet_pt)
 
         imgnet_nat_eps_list = []
